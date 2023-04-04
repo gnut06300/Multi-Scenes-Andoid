110a5657008b19756bf7d5083eb89c442f4a97d5
HarmoNick777 <harmonick777@gmail.com>
2023-04-04 13:37:35 +0200

    Joystick_Button_Added_Not_Functional

---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JumpController : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public bool isPressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+}
