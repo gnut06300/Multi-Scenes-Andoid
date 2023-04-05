c6151f54c36976ca1b5dd2126f105071e57ace7f
HarmoNick777 <harmonick777@gmail.com>
2023-04-05 10:15:10 +0200

    Commit2

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -13,33 +13,22 @@ public class CharacterMovementController : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] float groundCheckRadius;
     [SerializeField] LayerMask layerIsGround;
-    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
         jumpButton = FindObjectOfType<JumpController>();
-        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, layerIsGround);
-        /*if (isGrounded)
-        {
-            GetComponent<CharacterController>().enabled = false;
-        }*/
-        /*if (jumpButton.isPressed)
-        {
-            GetComponent<CharacterController>().enabled = false;
-        }*/
-        //rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
+        
         if (isGrounded && jumpButton.isPressed)
         {
-            GetComponent<CharacterController>().enabled = false;
-            //rb.velocity = Vector3.up * jumpForce;
+            Debug.Log("Yolo");
         }
     }
 }
